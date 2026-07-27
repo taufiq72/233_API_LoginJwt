ed2433ceb8cb3a84ccc5301531aebf4818561333
taufiq72 <taufiqq1207@gmail.com>
2026-07-27 09:47:29 +0700

    menambahkan async function getAllKomik

--- controller/komikcontroller.js
+++ controller/komikcontroller.js
@@ -1,1 +1,11 @@
 const { Komik } = require('../models');
+
+async function getAllKomik(req, res) {
+    try {
+        const komik = await Komik.findAll();
+        res.status(200).json(komik);
+    } catch (err) {
+        console.error('Error fetching komik:', err.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
