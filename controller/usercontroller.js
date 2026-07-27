b844613e2b0d76dcba25aa15daaa2e39bbcd1300
taufiq72 <taufiqq1207@gmail.com>
2026-07-27 11:05:47 +0700

    menambahkan usercontroller, authmiddleware, dan modifikasi api.js

---
+++ controller/usercontroller.js
@@ -0,0 +1,102 @@
+const db = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = db.User;
+
+async function register(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ 
+                message: 'Email and password wajib di isi' 
+            });
+        }
+
+        const existingUser = await User.findOne({
+            where: { email }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ 
+                message: 'Email sudah terdaftar' 
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: 'Registrasi berhasil',
+            user: {
+                id: user.id,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message 
+        });
+    }
+}
+
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ 
+                message: 'Email dan password wajib di isi' 
+            });
+        }
+
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        if (!user) {
+            return res.status(400).json({ 
+                message: 'Email atau password salah' 
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ 
+                message: 'Email atau password salah' 
+            });
+        }
+
+        const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        { 
+            expiresIn: process.env.JWT_EXPIRES 
+        }
+        );
+
+        res.status(200).json({
+            message: 'Login berhasil',
+            token
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message 
+        });
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { Komik } = require('../models');
-const komikController = require('../controller/komikcontroller'); // <-- TAMBAHKAN BARIS 
 
+const komikController = require('../controller/komikcontroller');
+const userController = require('../controller/usercontroller');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+
+//public
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
-router.post('/komik', komikController.createKomik);
-router.put('/komik/:id', komikController.updateKomik);
-router.delete('/komik/:id', komikController.deleteKomik);
+
+//protected
+router.post('/komik', authMiddleware, komikController.createKomik);
+router.put('/komik/:id', authMiddleware, komikController.updateKomik);
+router.delete('/komik/:id', authMiddleware, komikController.deleteKomik);
 
 module.exports = router;
