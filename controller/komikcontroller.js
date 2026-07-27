d3e46b03c0cfda8fa021a40ddfd24f21ba93143b
taufiq72 <taufiqq1207@gmail.com>
2026-07-27 09:47:56 +0700

    menambahkan async function getKomikById

--- controller/komikcontroller.js
+++ controller/komikcontroller.js
@@ -9,3 +9,17 @@ async function getAllKomik(req, res) {
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+async function getKomikById(req, res) {
+    const { id } = req.params;
+    try {
+        const komik = await Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).json({ error: 'Komik not found' });
+        }
+        res.status(200).json(komik);
+    } catch (err) {
+        console.error('Error fetching komik by ID:', err.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
