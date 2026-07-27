b2d688888aaf2ea2df93f9b9f73a76424bdef2d3
taufiq72 <taufiqq1207@gmail.com>
2026-07-27 09:49:34 +0700

    menambahkan async function deleteKomik

--- controller/komikcontroller.js
+++ controller/komikcontroller.js
@@ -62,3 +62,19 @@ async function updateKomik(req, res) {
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+async function deleteKomik(req, res) {
+    const { id } = req.params;
+    try {
+        const komik = await Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).json({ error: 'Komik not found' });
+        }
+
+        await komik.destroy();
+        res.status(200).json({ message: 'Komik deleted successfully' });
+    } catch (err) {
+        console.error('Error deleting komik:', err.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
